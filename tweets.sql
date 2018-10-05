3c35b5c405a719622074c72652c4ce4b4a7cf41d
metamn <bartus.csongor@gmail.com>
2018-10-05 16:40:01 +0300

    DDL done

--- tweets.sql
+++ tweets.sql
@@ -19,3 +19,14 @@ INSERT INTO users (name) VALUES ("Csongor");
 
 INSERT INTO tweets (text, date, user_id) VALUES ("This is Mark's tweet #1", now(), 1);
 INSERT INTO tweets (text, date, user_id) VALUES ("This is Csongor's tweet #1", now(), 2);
+
+INSERT INTO tweets (text, date, user_id) VALUES ("This is Mark's tweet #2", now(), 1);
+INSERT INTO tweets (text, date, user_id) VALUES ("This is Csongor's tweet #2", now(), 2);
+
+INSERT INTO tweets (text, date, user_id) VALUES ("This is Mark's tweet #3", now(), 1);
+INSERT INTO tweets (text, date, user_id) VALUES ("This is Csongor's tweet #3", now(), 2);
+
+INSERT INTO tweets (text, date, user_id) VALUES ("This is Mark's tweet #4", now(), 1);
+INSERT INTO tweets (text, date, user_id) VALUES ("This is Csongor's tweet #4", now(), 2);
+
+SELECT users.name, tweets.text, tweets.date FROM tweets INNER JOIN users ON users.user_id = tweets.user_id WHERE users.name = "Csongor" ORDER BY tweets.date ASC
