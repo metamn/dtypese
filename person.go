2825bb8ffdb436541935ec74d263c52fa942bfd2
metamn <bartus.csongor@gmail.com>
2018-10-05 12:23:22 +0300

    Working on the Go solution

--- person.go
+++ person.go
@@ -5,7 +5,22 @@ import (
 	"math/rand"
 )
 
+type Person struct {
+	Age int
+}
+
+func GeneratePeople(number, min, max int) []int {
+	ret := make([]int, number)
+
+	for i := 0; i < number; i++ {
+		ret[i] = rand.Intn(max-1) + min
+	}
+
+	return ret
+}
+
 func main() {
-	fmt.Println("The age of people in the list: ")
+	people := GeneratePeople(10, 1, 100)
+	fmt.Println("The age of people in the list: ", people)
 	fmt.Println("Has twice as old?: ")
 }
