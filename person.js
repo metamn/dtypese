31699986eac0a6d36073c79d5d14a01d3a5a05b0
metamn <bartus.csongor@gmail.com>
2018-10-05 11:05:55 +0300

    JS code done

--- person.js
+++ person.js
@@ -6,4 +6,13 @@ class Person {
 	static generatePeople(number, min, max) {
 		return [...Array(number).keys()].map(x => Math.floor(Math.random() * (max - min) + min));
 	}
+
+	static hasTwiceAsOld( people ) {
+		people.sort((a, b) => a > b);
+
+		console.log('The age of people in the list: ' + people);
+		console.log('Has twice as old?: ');
+
+		return ( 2 * people[0] <= people[people.length - 1]);
+	}
 }
