90d78656c9c51caed1c29f8c5a3a8895c06e0ea3
metamn <bartus.csongor@gmail.com>
2018-10-06 11:55:23 +0300

    Working on the React code

--- 10-time.js
+++ 10-time.js
@@ -3,87 +3,94 @@
 const e = React.createElement;
 
 class TimeSinceApp extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
     
-    this.state = {
-        timeStamp: '',
-        timeSince: {
-            years: '',
-            months: '',
-            days: '',
-            hours: '',
-            minutes: '',
-            seconds: ''
-        }
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  render() {
-    return (
-        <div>
-            <h3>Time since</h3>
-
-            <ol>
-                <li>How many years, months, days, hours, minutes and seconds that have elapsed since the timestamp</li>
-                <li>The elapsed time should update in real-time</li>
-            </ol>
-
-            <TimeSinceResult item={this.state} />
-                
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="new-timestamp">
-                    Enter UNIX timestamp: 
-                </label>
-
-                <input
-                    id="new-timestamp"
-                    onChange={this.handleChange}
-                    value={this.state.timeStamp}
-                />
-                
-                <button>
-                    Add
-                </button>
-            </form>
-        </div>
-    );
-  }
-
-  handleChange(e) {
-      this.setState({ timeStamp: e.target.value });
-  }
-
-  handleSubmit(e) {
-      e.preventDefault();
-      
-      if (!this.state.timeStamp.length) {
-          return;
-      }
-      
-      this.setState(state => ({
-        timeSince: this.getTimeSince()
-      }));
-  }
-
-  
-  // Displays a timestamp.
-  getTimeSince() {
-    var ts = moment("20080627");
-    var now = moment(new Date());
-    var diff = moment.duration(now.diff(ts));
-
-    return {
-        years: diff.years(),
-        months: diff.months(),
-        days: diff.days(),
-        minutes: diff.minutes(),
-        seconds: diff.seconds()
+        this.state = {
+            timeStamp: '',
+            timeHuman: '',
+            timeSince: {
+                years: '',
+                months: '',
+                days: '',
+                hours: '',
+                minutes: '',
+                seconds: ''
+            }
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  }
+
+    render() {
+        return (
+            <div>
+                <h3>Time since</h3>
+
+                <ol>
+                    <li>How many years, months, days, hours, minutes and seconds that have elapsed since the timestamp</li>
+                    <li>The elapsed time should update in real-time</li>
+                </ol>
+
+                <TimeSinceResult item={this.state} />
+                    
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="new-timestamp">
+                        Enter UNIX timestamp (like 1538806687): 
+                    </label>
+
+                    <input
+                        id="new-timestamp"
+                        onChange={this.handleChange}
+                        value={this.state.timeStamp}
+                    />
+                    
+                    <button>
+                        Add
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        this.setState({ timeStamp: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+      
+        if (!this.state.timeStamp.length) {
+            return;
+        }
+      
+        this.setState(state => ({
+            timeHuman: this.getTimeHuman(),
+            timeSince: this.getTimeSince()
+        }));
+    }
+
+
+    // Makes a UNIX timestamp human readable 
+    getTimeHuman() {
+        return moment.unix(this.state.timeStamp).format("MMMM Do YYYY, h:mm:ss a");
+    }
+  
+    // Displays a timestamp.
+    getTimeSince() {
+        var ts = moment(this.state.timeStamp, 'X');
+        var now = moment(new Date());
+        var diff = moment.duration(now.diff(ts));
+
+        return {
+            years: diff.years(),
+            months: diff.months(),
+            days: diff.days(),
+            minutes: diff.minutes(),
+            seconds: diff.seconds()
+        }
+    }
 }
 
 class TimeSinceResult extends React.Component {
@@ -92,7 +99,7 @@ class TimeSinceResult extends React.Component {
             return (
                 <dl>
                     <dt>Timestamp</dt>
-                    <dd>{this.props.item.timeStamp}</dd>
+                    <dd>{this.props.item.timeStamp} - {this.props.item.timeHuman}</dd>
                     <dt>Time passed since:</dt>
                     {Object.entries(this.props.item.timeSince).map(item => (
                         <dd>{item[0]} - {item[1]}</dd>
